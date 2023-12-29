d0ca1de0344584790319097c6a2fef25acd3c540
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2023-12-29 18:23:03 +0900

    アカウント削除実装

--- php/content/account_del_confirm.php
+++ php/content/account_del_confirm.php
@@ -1,17 +1,15 @@
 <?php
     // Header部分共通
     require_once(dirname(__FILE__).'/./header/header.php');
+    
 
-   
-    $user_id = $auth = $type = 0;
-    $passwd = $user_name = "";
-
+    $user = $passwd = $admin = $manager = "";
     $user_id = $_GET['user_id'];
 
-    //DB検索
+    //DB TABLEから読み出し
     $tblName = "account_tbl";
-    $where   = "user_id='".$user_id."'";
-    $ret = readTbl($tblName, $where, NULL, NULL, NULL);
+    $param = 'user_id ='.$user_id;
+    $ret = readTbl($tblName, $param, NULL, NULL, NULL);
     if ($ret != FALSE) {
         foreach ($ret as $value) {
             $user_name = $value['user_name'];
@@ -37,6 +35,32 @@
     <?php echo $strHeader; ?>
     <br>
     <div class="block ml-6">
+        <p>削除しますか？</p>
+    </div>
+
+    <div class="block ml-6 mr-6">
+        <form action="account_del_done.php" method="post">
+            <input type="hidden" name="user_id" value="<?php echo $user_id;?>">
+
+            <div class="field">
+                <p>delete と入力してください</p>
+                <div class="control">
+                    <input class="input is-sucess" type="text" name="key_word">
+                </div>
+            </div>
+            <div class="field is-grouped">
+                <a href="<?php echo $strBack; ?>">
+                    <span class="button has-background-grey-lighter">戻る</span>
+                </a>
+                <div class="control ml-6">
+                    <input class="button is-danger" type="submit" value="削除実行">
+                </div>
+            </div>            
+        </form>
+    </div>
+
+    <br>
+    <div class="block ml-6">
         <table class="table" >
             <tr>
                 <td>社員番号</td>
@@ -51,20 +75,6 @@
                 <td><?php echo $auth;?></td>
             </tr>
         </table>
-    </div>
-
-    <div class="block ml-6">
-        <a href="branch.php?account_edit_type=edit&account_idx=<?php echo $account_idx;?>">
-            <span class="button has-background-grey-lighter">編集</span>
-        </a>
-<!---
-        <a href="branch.php?account_edit_type=passwd_clr&account_idx=<?php echo $account_idx;?>">
-            <span class="button has-text-light has-background-danger ml-5">パスワード初期化</span>
-        </a>
----> 
-        <a href="account_del_confirm.php?user_id=<?php echo $user_id;?>">
-            <span class="button has-text-light has-background-danger ml-5">アカウント削除</span>
-        </a> 
     </div>
 
     <?php include(dirname(__FILE__).'/./header/bulma_burger.js'); ?>
