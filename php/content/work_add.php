45d588a91106b55efe6ac996c8afa786b0c3a63b
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2023-12-30 13:02:12 +0900

    作業項目一覧と追加登録を実装

---
+++ php/content/work_add.php
@@ -0,0 +1,51 @@
+<?php
+    // Header部分共通
+    require_once(dirname(__FILE__).'/./header/header.php');
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<?php include(dirname(__FILE__).'/./header/head_html.php'); ?>
+<body>
+    <?php echo $strHeader; ?>
+    <br>
+    <div class="block ml-6 mr-6">
+        <form action="work_add_done.php" method="POST">
+            <div class="field">
+                <label class="label">作業番号</label>
+                <div class="control">
+                    <input class="input is-sucess" type="text" name="work_id" required>
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">作業名</label>
+                <div class="control">
+                    <input class="input is-sucess" type="text" name="work_name" required>
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">直接/間接</label>
+                <div class="control">
+                    <div class="select is-success">
+                        <select name="direct">
+                            <option value="0" selected>直接</option>
+                            <option value="1">間接</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="field is-grouped">
+                <div class="control">
+                    <input class="button has-background-grey-lighter" type="reset" value="取消">
+                </div>
+                <div class="control">
+                    <input class="button is-success ml-4" type="submit" value="登録">
+                </div>
+            </div>
+        </form> 
+    </div>
+
+    <?php include(dirname(__FILE__).'/./header/bulma_burger.js'); ?>
+</body>
+</html>
---
+++ php/content/work_add_done.php
@@ -0,0 +1,76 @@
+<?php
+    // Header部分共通
+    require_once(dirname(__FILE__).'/./header/header.php');
+
+    //e()用
+    require_once(dirname(__FILE__).'/./common/Encode.php');
+
+
+    //２重登録の確認
+    {
+        $errFlag = FALSE;      
+        $work_id = e($_POST['work_id']);
+
+        //DB検索
+        $tblName = "work_tbl";
+        $where   = "work_id='".$work_id."'";
+        $ret = readTbl($tblName, $where, NULL, NULL, NULL);
+        if ($ret != FALSE) {
+            $errFlag = TRUE;
+            $result = "作業番号:".$work_id." は、既に登録されています。";
+        }
+    }
+
+
+    //登録
+    if ($errFlag == FALSE) {
+
+        //DB TABLEの要素名リスト
+        $keyName = ['work_id','work_name','direct','job_type'];
+        $keyValue = [];
+    
+        //DB TABLEの 要素名:値 になるよう連想配列を作成
+        foreach ($keyName as $key) {
+            if (($key == 'work_id') || ($key == 'direct')) {
+                $keyValue[$key] = (int)e($_POST[$key]);
+            } elseif ($key == 'job_type') {
+                $keyValue[$key] = 0;
+            } else {
+                $keyValue[$key] = e($_POST[$key]);
+            }
+        }
+        
+        //DB TABLEへ書き込み
+        $tblName = "work_tbl";
+        if (writeTbl($tblName, $keyValue) == TRUE) {
+            $result = "登録しました。";
+        } else {
+            $result = "登録が失敗しました。";
+        }    
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<?php include(dirname(__FILE__).'/./header/head_html.php'); ?>
+<body>
+    <?php echo $strHeader; ?>
+    <br>
+    <br>
+    <div class="block ml-6">
+        <p><?php echo $result; ?></p>
+    </div>
+    <div class="block ml-6">
+        <a href="work_list.php">作業項目へ</a>
+    </div>
+<!---
+    <script>
+        setTimeout(function() {
+            window.location.href = "work_list.php";
+        }, 2*1000);
+    </script>
+--->
+
+    <?php include(dirname(__FILE__).'/./header/bulma_burger.js'); ?>
+</body>
+</html>
