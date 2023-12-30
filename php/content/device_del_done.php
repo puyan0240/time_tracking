bcc717e8fb641e7039fbd5674b2846e52e1f448f
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2023-12-30 10:48:17 +0900

    機種削除を実装

---
+++ php/content/device_del_done.php
@@ -0,0 +1,50 @@
+<?php
+    // Header部分共通
+    require_once(dirname(__FILE__).'/./header/header.php');
+
+    //e()用
+    require_once(dirname(__FILE__).'/./common/Encode.php');
+
+
+    $result = "削除できませんでした。";
+
+    if ($_POST['key_word'] == "delete") { //削除用キーワード[delete]
+        
+        //DB TABLEの要素名リスト
+        $paramKeyName = ['device_id'];
+        $paramKeyValue = [];
+
+        //DB TABLEの 要素名:値 になるよう連想配列を作成
+        foreach ($paramKeyName as $key) {
+            $paramKeyValue[$key] = e($_POST[$key]);
+        }
+
+        //DB TBLを更新
+        $tblName = "device_tbl";
+        if (deleteTbl($tblName, $paramKeyValue) == TRUE) {
+            $result = "削除しました。";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<?php include(dirname(__FILE__).'/./header/head_html.php'); ?>
+<body>
+    <?php echo $strHeader; ?>
+    <br>
+    <div class="block ml-6">
+        <p><?php echo $result; ?></p>
+    </div>
+    <div class="block ml-6">
+        <a href="device_list.php">機種管理</a>
+    </div>
+    <script>
+        setTimeout(function() {
+            window.location.href = "device_list.php";
+        }, 2*1000);
+    </script>
+
+    <?php include(dirname(__FILE__).'/./header/bulma_burger.js'); ?>
+</body>
+</html>
---
+++ php/content/device_detail.php
@@ -0,0 +1,59 @@
+<?php
+    // Header部分共通
+    require_once(dirname(__FILE__).'/./header/header.php');
+
+    $ver = $device_name = "";
+    $device_id = $_GET['device_id'];
+
+    //DB検索
+    $tblName = "device_tbl";
+    $where   = "device_id='".$device_id."'";
+    $ret = readTbl($tblName, $where, NULL, NULL, NULL);
+    if ($ret != FALSE) {
+        foreach ($ret as $value) {
+            $ver         = $value['ver'];
+            $device_name = $value['device_name'];
+        }
+    }
+
+    //戻り先
+    $strBack = $_SERVER['HTTP_REFERER'];
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<?php include(dirname(__FILE__).'/./header/head_html.php'); ?>
+<body>
+    <?php echo $strHeader; ?>
+    <br>
+    <div class="block ml-6">
+        <table class="table" >
+            <tr>
+                <td>機種番号</td>
+                <td><?php echo $device_id; ?></td>
+            </tr>
+            <tr>
+                <td>Ver</td>
+                <td><?php echo $ver; ?></td>
+            </tr>
+            <tr>
+                <td>商品名:</td>
+                <td><?php echo $device_name;?></td>
+            </tr>
+        </table>
+    </div>
+
+    <div class="block ml-6">
+<!---
+        <a href="account_edit.php?user_id=<?php echo $user_id;?>">
+            <span class="button has-background-grey-lighter">編集</span>
+        </a>
+--->
+        <a href="device_del_confirm.php?device_id=<?php echo $device_id;?>">
+            <span class="button has-text-light has-background-danger ml-5">機種削除</span>
+        </a> 
+    </div>
+
+    <?php include(dirname(__FILE__).'/./header/bulma_burger.js'); ?>
+</body>
+</html>
