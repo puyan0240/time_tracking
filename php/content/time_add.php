4d7dd1256ef1582aa472fcd0671315595a7a7683
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2023-12-31 15:27:21 +0900

    時間登録　実装中

--- php/content/time_add.php
+++ php/content/time_add.php
@@ -1,6 +1,72 @@
 <?php
     // Header部分共通
     require_once(dirname(__FILE__).'/./header/header.php');
+
+    //機種一覧
+    {
+        //DB TABLEから読み出し
+        $tblName = "device_tbl";
+        $device_list = readTbl($tblName, NULL, NULL, NULL, NULL);
+    }
+
+    //作業項目一覧
+    {
+        //DB TABLEから読み出し
+        $tblName = "work_tbl";
+        $work_list = readTbl($tblName, NULL, NULL, NULL, NULL);
+    }
+
+
+    {
+        //時間
+        {
+            $strHourSelOpt = "";
+            $format = "<option value=\"%s\" selected>%02s</option>";
+            for ($i = 0; $i <= 12; $i ++) {
+                $strHourSelOpt .= sprintf($format, $i, $i);
+            }
+
+            $format = 
+            "<div class=\"field\">
+                <div class=\"control\">
+                    <div class=\"select is-success\">
+                        <select name=\"hour\">
+                            %s
+                        </select>
+                    </div>
+                </div>
+            </div>";
+            $strHourSel = sprintf($format, $strHourSelOpt);
+        }
+
+        //分
+        {
+            $strMinSelOpt = "";
+            $format = "<option value=\"%s\" selected>%02s</option>";
+            for ($i = 0; $i < 60; $i = $i+15) {
+                $strMinSelOpt .= sprintf($format, $i, $i);
+            }
+
+            $format = 
+            "<div class=\"field\">
+                <div class=\"control\">
+                    <div class=\"select is-success\">
+                        <select name=\"min\">
+                            %s
+                        </select>
+                    </div>
+                </div>
+            </div>";
+            $strMinSel = sprintf($format, $strMinSelOpt);
+
+        }
+    }
+
+    /*
+    var_dump($device_list);
+    print("\n");
+    var_dump($work_list);
+    */
 ?>
 
 <!DOCTYPE html>
@@ -9,76 +75,30 @@
 <body>
     <?php echo $strHeader; ?>
     <br>
+
     <div class="block ml-6 mr-6">
-        <form action="history_add_confirm.php" method="POST">
-            <div class="field">
-                <label class="label">日付</label>
-                <div class="control">
-                    <input class="input is-sucess" type="text" name="date" required value="<?php echo date('Y-m-d'); ?>">
-                </div>
+        <form action="time_add_done.php" method="POST">
+  
+            <div class="block ml-6">
+                <table class="table" id="list_table">
+                    <tr>
+                        <th>機種</th>
+                        <th>作業内容</th>
+                        <th>時間</th>
+                        <th>分</th>
+                    </tr>
+
+                    <?php echo $strHourSel; ?>
+                    <?php echo $strMinSel; ?>
+                </table>
             </div>
 
-            <div class="field">
-                <label class="label">マシン名</label>
-                <div class="control">
-                    <div class="select is-success">
-                        <select name="item_idx">
-                            <?php echo $strSelectItem; ?>
-
-                        </select>
-                    </div>
-                </div>
-            </div>
-
-            <div class="columns">
-                <div class="column is-narrow">
-                    <label class="label">1回目</label>
-                </div>
-                <div class="column is-narrow">
-                    <input class="input is-sucess" type="text" name="weight_1" required value="">
-                </div>
-                <div class="column is-narrow">kg</div>
-                <div class="column is-narrow">
-                    <input class="input is-sucess" type="text" name="count_1" required value="">
-                </div>
-                <div class="column is-narrow">回</div>
-            </div>
-
-            <div class="columns">
-                <div class="column is-narrow">
-                    <label class="label">2回目</label>
-                </div>
-                <div class="column is-narrow">
-                    <input class="input is-sucess" type="text" name="weight_2" value="">
-                </div>
-                <div class="column is-narrow">kg</div>
-                <div class="column is-narrow">
-                    <input class="input is-sucess" type="text" name="count_2" value="">
-                </div>
-                <div class="column is-narrow">回</div>
-            </div>
-
-            <div class="columns">
-                <div class="column is-narrow">
-                    <label class="label">3回目</label>
-                </div>
-                <div class="column is-narrow">
-                    <input class="input is-sucess" type="text" name="weight_3" value="">
-                </div>
-                <div class="column is-narrow">kg</div>
-                <div class="column is-narrow">
-                    <input class="input is-sucess" type="text" name="count_3" value="">
-                </div>
-                <div class="column is-narrow">回</div>
-            </div>
-
-            <br>
             <div class="field is-grouped">
                 <div class="control">
                     <input class="button has-background-grey-lighter" type="reset" value="取消">
                 </div>
                 <div class="control">
-                    <input class="button is-success ml-4" type="submit" value="登録確認">
+                    <input class="button is-success ml-4" type="submit" value="登録">
                 </div>
             </div>
         </form> 
