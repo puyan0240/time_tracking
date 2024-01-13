841ff7a79ce312e649b895e777ef600518003481
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2024-01-13 23:55:19 +0900

    DB構成更新

--- mysql/time_tracking_db.sql
+++ mysql/time_tracking_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: db
--- 生成日時: 2024 年 1 月 06 日 06:56
+-- 生成日時: 2024 年 1 月 13 日 00:46
 -- サーバのバージョン： 8.2.0
 -- PHP のバージョン: 8.2.13
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `account_tbl` (
-  `user_id` int NOT NULL,
+  `user_id` int UNSIGNED NOT NULL,
   `passwd` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `user_name` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `auth` int NOT NULL,
@@ -42,9 +42,9 @@ CREATE TABLE `account_tbl` (
 --
 
 CREATE TABLE `device_tbl` (
-  `idx` int NOT NULL,
-  `device_id` int NOT NULL,
-  `ver` int NOT NULL,
+  `idx` int UNSIGNED NOT NULL,
+  `device_id` int UNSIGNED NOT NULL,
+  `ver` int UNSIGNED NOT NULL,
   `device_name` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
@@ -55,11 +55,12 @@ CREATE TABLE `device_tbl` (
 --
 
 CREATE TABLE `time_traking_tbl` (
-  `idx` int NOT NULL,
+  `idx` int UNSIGNED NOT NULL,
   `date` date NOT NULL,
-  `user_id` int NOT NULL,
-  `device_id` int NOT NULL,
-  `work_id` int NOT NULL,
+  `user_id` int UNSIGNED NOT NULL,
+  `device_tbl_idx` int UNSIGNED NOT NULL,
+  `work_id` int UNSIGNED NOT NULL,
+  `ref_device_tbl_idx` int UNSIGNED NOT NULL,
   `time` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
@@ -70,7 +71,7 @@ CREATE TABLE `time_traking_tbl` (
 --
 
 CREATE TABLE `work_tbl` (
-  `work_id` int NOT NULL,
+  `work_id` int UNSIGNED NOT NULL,
   `work_name` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `direct` int NOT NULL,
   `job_type` int NOT NULL
@@ -112,13 +113,13 @@ ALTER TABLE `work_tbl`
 -- テーブルの AUTO_INCREMENT `device_tbl`
 --
 ALTER TABLE `device_tbl`
-  MODIFY `idx` int NOT NULL AUTO_INCREMENT;
+  MODIFY `idx` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- テーブルの AUTO_INCREMENT `time_traking_tbl`
 --
 ALTER TABLE `time_traking_tbl`
-  MODIFY `idx` int NOT NULL AUTO_INCREMENT;
+  MODIFY `idx` int UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
