b64213bdf39f00cc9856c52dd46f7e9c32056f8f
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2024-01-15 18:28:51 +0900

    同じなら纏める

--- php/content/time_add_done.php
+++ php/content/time_add_done.php
@@ -26,43 +26,76 @@
     {
         $result = "登録しました。";
 
+        $inputArray = [];
+        $inputName = ['device_tbl_idx','work_id','hour','min'];
+    
+        //POST値を取得
         for ($i = 0; $i < 12; $i ++) {
 
             //Inputキーリスト
-            $inputName = ['device_tbl_idx','work_id','hour','min'];
-            $inputValue = [];
+            $value = [];
 
             //Input値を取り出す
             foreach ($inputName as $name) {
                 $format = "%s%02s";
                 $strTmp = sprintf($format, $name, $i);
-                $inputValue[$name] = $_POST[$strTmp];
+                $value[$name] = $_POST[$strTmp];
             }
 
-            //作業時間が登録されている場合はDBへ登録する
-            if (($inputValue['hour'] == 0) && ($inputValue['min'] == 0)) {
+            $inputArray[$i] = $value;
+        }
+
+        //同じ機種&作業なら纏める
+        for ($i = 0; $i < 12; $i ++) {
+            if (($inputArray[$i]['hour'] == 0) && ($inputArray[$i]['min'] == 0)) {
+                continue; //登録なし
+            } else {
+                for ($j = $i+1; $j < 12; $j ++) {
+                    if (($inputArray[$j]['hour'] == 0) && ($inputArray[$j]['min'] == 0)) {
+                        continue; //登録なし
+                    } else {
+                        if ($inputArray[$i]['device_tbl_idx'] != $inputArray[$j]['device_tbl_idx']) {
+                            continue; //違う
+                        }
+                        if ($inputArray[$i]['work_id'] != $inputArray[$j]['work_id']) {
+                            continue; //違う
+                        }
+
+                        //纏める
+                        $inputArray[$i]['hour'] += $inputArray[$j]['hour'];
+                        $inputArray[$j]['hour'] = 0;
+
+                        $inputArray[$i]['min']  += $inputArray[$j]['min']; 
+                        $inputArray[$j]['min']  = 0;
+                    }
+                }
+            }
+        }
+
+        foreach ($inputArray as $value) {
+            if (($value['hour'] == 0) && ($value['min'] == 0)) {
                 continue; //登録なし
             } else {
                 //DB TABLEの 要素名:値 になるよう連想配列を作成
                 $keyValue = [];
                 $keyValue['date'] = $date;
                 $keyValue['user_id'] = (int)$_SESSION['user_id'];
-                $keyValue['device_tbl_idx'] = (int)($inputValue['device_tbl_idx']);
-                $keyValue['work_id'] = (int)($inputValue['work_id']);
+                $keyValue['device_tbl_idx'] = (int)($value['device_tbl_idx']);
+                $keyValue['work_id'] = (int)($value['work_id']);
                 $keyValue['ref_device_tbl_idx'] = 0; //
-
-                $time = (int)($inputValue['hour']) * 60;
-                $time += (int)($inputValue['min']);
+                
+                $time = (int)($value['hour']) * 60;
+                $time += (int)($value['min']);
                 $keyValue['time'] = $time;
-
-
+                
+                
                 //DB TABLEへ書き込み
                 $tblName = "time_traking_tbl";
                 if (writeTbl($tblName, $keyValue) == TRUE) {
                     ;
                 } else {
                     $result = "登録が失敗しました。";
-                }  
+                } 
             }
         }
     }
@@ -81,12 +114,13 @@
     <div class="block ml-6">
         <a href="time_add.php">時間登録へ</a>
     </div>
-
+<!--
     <script>
         setTimeout(function() {
             window.location.href = "time_add.php";
         }, 2*1000);
     </script>
+-->
     <?php include(dirname(__FILE__).'/./header/bulma_burger.js'); ?>
 </body>
 </html>
